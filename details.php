7a9d07227fc1f1fe8585ffe23b25e62452ed79b8
magtiwalakalang <44990270+magtiwalakalang@users.noreply.github.com>
2024-11-07 17:05:02 +0800

    Update details.php

--- details.php
+++ details.php
@@ -29,7 +29,7 @@ $con = connection();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-cpmpatible"content="ie=edge">
     <title>DRWSA Management System</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
 
 </head>
 <body>
